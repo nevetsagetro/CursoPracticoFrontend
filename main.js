e551fae8b8e19f3abd8cfc784de9a7d48b461e77
nevetsagetro <nvs.agt@gmail.com>
2022-08-12 16:28:36 +0200

    C20 - Carrito de compras y toggle

--- main.js
+++ main.js
@@ -2,14 +2,38 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuBurgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const asideProductDetail = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuBurgerIcon.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleAsideProductDetail);
 
 function toggleDesktopMenu() {
+  isAsideProductDetailClosed =
+    asideProductDetail.classList.contains("inactive");
+
+  if (!isAsideProductDetailClosed) {
+    asideProductDetail.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive"); //classList.toggle("inactive"); - Quita o pone la clase inactive dependiendo si latiene o no.
 }
 
 function toggleMobileMenu() {
+  const isAsideProductDetailClosed =
+    asideProductDetail.classList.contains("inactive");
+  if (!isAsideProductDetailClosed) {
+    asideProductDetail.classList.add("inactive");
+  }
   mobileMenu.classList.toggle("inactive"); //classList.toggle("inactive"); - Quita o pone la clase inactive dependiendo si latiene o no.
+}
+
+function toggleAsideProductDetail() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  asideProductDetail.classList.toggle("inactive");
 }
